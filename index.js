f2735e8cad095878604f6464dd3394155ca249e9
andrewcreekmore <andrew.creekmore@me.com>
2021-10-09 15:59:44 -0500

    updated css

--- index.js
+++ index.js
@@ -2,25 +2,13 @@
 // CS 361 
 // Fall 2021
 
+// setup and routing
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-const got = require('got')
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-const heroku_url= 'https://mighty-beyond-55909.herokuapp.com/';
-
-got(heroku_url).then(response => {
-  const dom = new JSDOM(response.body);
-  console.log(dom.window.document.querySelector('title').textContent);
-}).catch(err => {
-  console.log(err);
-});
 
 // define default route
 app.get('/', (req, res) => res.render('index'))
@@ -36,29 +24,10 @@ function addTask(text) {
     const task = {
         text,
         checked: false,
+        // create unique yet arbitrary id value
         id: Date.now(),
     };
 
     tasks.push(task);
     console.log(tasks);
 }
-
-
-
-    //  // select form element
-    //  const form = document.querySelector('.js-form');
-    //  // add submit event listener
-    //  form.addEventListener('submit', event =>{
-    //      // no page auto-refresh on submit
-    //      event.preventDefault();
-    //      // select text input
-    //      const input = document.querySelector('.js-task-input');
-     
-    //      // get input value
-    //      const text = input.value.trim();
-    //      if (text !== '') {
-    //          addTask(text);
-    //          input.value = '';
-    //          input.focus();
-    //      }
-    //  });
