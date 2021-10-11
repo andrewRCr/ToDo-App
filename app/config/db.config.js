d04ce9489a343ae42762d6a9b97a17bb206a5926
andrewcreekmore <andrew.creekmore@me.com>
2021-10-11 13:14:00 -0500

    db setup

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb"
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "ba1ff23f712881",
+    PASSWORD: "6b703281",
+    DB: "heroku_c117fdf5a23974d"
   };
+
+
+  'mysql://ba1ff23f712881:6b703281@us-cdbr-east-04.cleardb.com/heroku_c117fdf5a23974d?reconnect=true'
