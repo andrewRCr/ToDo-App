d3bab0074b562edfe26b5ef159c3bbaf169e86a3
andrewcreekmore <andrew.creekmore@me.com>
2021-10-12 15:42:24 -0500

    ...

--- app.js
+++ app.js
@@ -25,7 +25,41 @@ app.get('/', (req, res) => res.render('index'))
 
 require("./app/routes/entry.routes.js")(app);
 
+// RABBITMQ SETUP (for assignment 3, temporary)
+// This is timer producer
+const INTERVAL_DURATION = 60000;
+
+
+const url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+const amqp = require('amqplib');
+
+exports.startTimer = () => {
+    amqp.connect(url).then((connection) => {
+        connection.createChannel().then((channel) => {
+            const exchange = 'quote';
+            channel.assertExchange(exchange, 'direct', {durable: false});
+            // channel.assertQueue(quote_queue, { durable: true});
+            setInterval(() => {
+                const message = "Let go get the quote";
+                console.log('send to queue', message);
+                // channel.sendToQueue(quote_queue, Buffer.from(message),  {
+                //     persistent: true
+                // });
+                channel.publish(exchange, '', Buffer.from(message));
+            }, INTERVAL_DURATION)
+        });
+    }).catch((err) => {
+        console.error('Connect amqp failed', err);
+    })
+}
+
+
+
+
 app.listen(port, () => console.log(`App listening at http://localhost:${port}; ctrl + C to stop.`));
+
+
+
 
 
 // TODO APP
