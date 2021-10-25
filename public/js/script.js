2b267efcb495cb2f560990e2f70f1cbd8820ff78
Andrew Creekmore <andrew.creekmore@me.com>
2021-10-25 16:10:06 -0500

    ...

--- public/js/script.js
+++ public/js/script.js
@@ -68,7 +68,7 @@ function renderTodos(todos) {
         </li> */
 
     // if item is completed, then add a class to <li> called 'checked', which will add line-through style
-    if (item.completed == true) {
+    if (item.completed === true) {
       li.classList.add('checked');
     }
 
@@ -117,5 +117,52 @@ function getFromLocalStorage() {
   }
 }
 
+// toggle the value to completed and not completed
+function toggle(id) {
+  todos.forEach(function(item) {
+    // use == not ===, because here types are different. One is number and other is string
+    if (item.id == id) {
+      // toggle the value
+      item.completed = !item.completed;
+    }
+  });
+
+  addToLocalStorage(todos);
+}
+
+// deletes a todo from todos array, then updates localstorage and renders updated list to screen
+function deleteTodo(id) {
+  // filters out the <li> with the id and updates the todos array
+  todos = todos.filter(function(item) {
+    // use != not !==, because here types are different. One is number and other is string
+    return item.id != id;
+  });
+
+  // update the localStorage
+  addToLocalStorage(todos);
+}
+
 // initially get everything from localStorage
 getFromLocalStorage();
+
+// after that addEventListener <ul> with class=todoItems. Because we need to listen for click event in all delete-button and checkbox
+todoItemsList.addEventListener('click', function(event) {
+  // check if the event is on checkbox
+  if (event.target.type === 'checkbox') {
+    // toggle the state
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+
+  // check if that is a delete-button
+  if (event.target.classList.contains('delete-button')) {
+    // get id from data-key attribute's value of parent <li> where the delete-button is present
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+
+    // // check if that is a date-button push 
+    // if (event.target.classList.contains('date-button')) {
+    //   // get id from data-key attribute's value of parent <li> where the delete-button is present
+    //   pushDatedTodo(event.target.parentElement.getAttribute('data-key'));
+    // }
+});
+
